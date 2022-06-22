b82c106e61fc632249a9871f1a33b79257943ed5
ItsSitanshu <sitanshu15shrestha@gmail.com>
2022-06-22 21:38:02 +0545

    Complete list [init_list, list_push] for now

---
+++ src/include/list.h
@@ -0,0 +1,13 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct LIST_STRUCT {
+    void** items;
+    size_t size;
+    size_t item_size;
+} list_T;
+
+list_T* init_list(size_t item_size);
+void list_push(list_T* list, void* item);
+
+#endif
